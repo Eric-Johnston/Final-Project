3751e17bb7ab902c51468b0ab791a27719eb9a6f
Eric J <ejohnston092@gmail.com>
2018-09-29 11:38:38 -0400

    branch rollback

---
+++ client/src/components/Search/Search.js
@@ -0,0 +1,11 @@
+import React, {component} from "react";
+import './App.css';
+import image from './reel.png';
+
+class Search extends Component {
+    render() {
+        return (
+            <App></App>
+        )
+    }
+}
